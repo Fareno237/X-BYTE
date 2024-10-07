0d324ccc1630d01bee0eab89680c09d54a37507a
Fareno237 <lerfareno@gmail.com>
2024-10-07 23:58:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,17 +5,17 @@ return text === fault ? true : false;
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTU1MWXUwbDlqcE44azdzU3FsdzkyMk5GbytWazZrVC9QbnRkOXArL3htcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMXIwQktjOHBzRzZHYnp0c293VGJ2SkorcU94dUJmZUU0QkNIYTJudVExOD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDSWN3bmloWXVld2UzR2EzbUIvVkVFMmNZTmV1MStPRlUrU0xDajczVVZvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnNFlTYitBWFQ0RWVnNTUyNERWSnlJYkVVdmtybEY1SG5tZEVvMDNkL253PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IktDejJwUXI2NTVqQm95MXRUcVAvbUxLNDlSdUhQdGp0WlNGanZyYUJMMkk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZVT3Q2bHpoenpTZkgvRnNUTHNOUXFkNWZERldmRld6cWk1ZHZHa1JQVFk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUxYbXlpTXQyVHo4KzBFMmtvV0pka2tBc290LzVxbFZIMEE2aXRkN3BIZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRWw0N2R2bkhiTHg3RWNpOTIwVWs3QjRnRFVaVzZVQ0NSSVU0R3c4ZXF3MD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkFOWmw3Sk94YnlEZlFMclhYaThtamErYUt3bXBTZk1EaW1NTHBITGtqSXVzKy9LRlh4blNkSDZ2c1FvRnJnSGhUbmMwMHoxSXZySlI1VlRrM3UyVURRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTI2LCJhZHZTZWNyZXRLZXkiOiJUSVFWYUZqdnFWKzdpSlJiY0xnenJlaVg5TkFiSDhTSFJ0dlZjc250eUt3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJWUGpjcWprY1EtdUEyR1RPTUxycUlnIiwicGhvbmVJZCI6ImQxNWZkMTBkLTA4ZDQtNDExYy05ODIwLWNjYmNhOTgwMWU4MCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKcWhlNXllckpDZzZmN3hCYysrOGttTDlaWWc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib1N2VVRVdmx0d0NPYXRZbE5qN09kdEs1QzRZPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ikw4N0FMMzkxIiwibWUiOnsiaWQiOiIyMzc2NTA1NjQ0NDU6OTdAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0xHNndmWUNFSmUra2JnR0dDTWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IkMrbzE3QVhVMy9XQlJXUjF6VDIrV1dGMUVJZ2FWQnhpVERlaC91R2hpU009IiwiYWNjb3VudFNpZ25hdHVyZSI6Imc2bmtmSWZuelBydkRHVWplVHRSdXFqSDZmWEdLRUtHU3p1Y3NqZ2dPeFJDd24vcHFxL0h4TGVXS05LeHdQVCtiWU9meFMreDVxY0R2NXppWUZQMENRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJZdkxVY0h1NXhDWnk3S0tMd0sxdSttWlZJVnlqbDhxenRCODdxR21GUlI2ejBNdFozYUk5VDB3YmZ6NWt5S2UyZWNGVXNzYTZxN2pJTitmbmFKTVREdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNzY1MDU2NDQ0NTo5N0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJRdnFOZXdGMU4vMWdVVmtkYzA5dmxsaGRSQ0lHbFFjWWt3M29mN2hvWWtqIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzI4MzM5NzQ4fQ==' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '237650564445' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,
-PREFIX: process.env.PREFIX || '.' ,
+PREFIX: process.env.PREFIX || '!' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9' : process.env.POSTGRESQL_URL,
 MAX_SIZE: 500, 
-ALIVE:  process.env.ALIVE  || ''  ,
-FOOTER: process.env.FOOTER=== undefined ? '> Created by Hamza': process.env.FOOTER,
-OWNER_REACT:  process.env.OWNER_REACT  || true  ,
+ALIVE:  process.env.ALIVE  || 'Yooo, ça va ? Indisponible actu mais je reviens.'  ,
+FOOTER: process.env.FOOTER=== undefined ? '> Made by FARENO': process.env.FOOTER,
+OWNER_REACT:  process.env.OWNER_REACT  || false  ,
 ADMIN_EVENT:  process.env.ADMIN_EVENT  || true  ,   
 AUTO_BLOCK:  process.env.AUTO_BLOCK  || false  ,
 AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
@@ -44,6 +44,6 @@ ANTI_DELETE : process.env.ANTI_DELETE || true ,
 // BLOCK_COUNTRY_PREFIX : process.env.BLOCK_COUNTRY_PREFIX || "1",
 BOT_EXPIRY_DATE : process.env.BOT_EXPIRY_DATE || '2029-09-05',
 BOT_EXPIRY_TIME : process.env.BOT_EXPIRY_TIME || '16:24:00',
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '237650564445' : process.env.DELETEMSGSENDTO,
   
 };
